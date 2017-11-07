fb4df40722a73eaac549a6d501d570905f0aff6e
Roman Sokolenko <roman.s1990@gmail.com>
2017-11-07 12:05:18 -0900

    Refactored response() call

--- lambda_function.py
+++ lambda_function.py
@@ -77,12 +77,11 @@ def lambda_handler(event, context):
     channel = params['channel_name'][0]
     command_text = params['text'][0]
 
-    if command_text == "Hello":
-        return respond(None, "Hi there, how are you?")
-    elif command_text == "I'm fine":
-        return respond(None, "That's good, only fine?")
-    else:
-        return respond(None,
-        'Hi, I am the EchoBot. You can send the following messages that I will understand:\n"Hello"\n "I\'m fine"')
+    response_string = 'Hi, I am the EchoBot. You can send the following messages that I will understand:\n"Hello"\n "I\'m fine"'
 
-    return respond(None, "%s invoked %s in %s with the following text: %s" % (user, command, channel, command_text))
+    if command_text == "Hello":
+        response_string = "Hi there, how are you?"
+    elif command_text == "I'm fine":
+        response_string = "That's good, only fine?"
+
+    return respond(None, response_string)
